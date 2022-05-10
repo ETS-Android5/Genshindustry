0b187777f281992562e785b75b70ceaa214d7c0e
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2022-05-10 13:38:24 +0700

    core

--- core/src/mindustry/mod/ModListing.java
+++ core/src/mindustry/mod/ModListing.java
@@ -2,7 +2,7 @@ package mindustry.mod;
 
 /** Mod listing as a data class. */
 public class ModListing{
-    public String repo, name, subtitle, author, lastUpdated, branch, description,  minGameVersion;
+    public String repo, name, subtitle, author, lastUpdated, description,  minGameVersion;
     public boolean hasScripts, hasJava;
     public String[] contentTypes = {};
     public int stars;
@@ -18,7 +18,6 @@ public class ModListing{
         ", minGameVersion='" + minGameVersion + '\'' +
         ", hasScripts=" + hasScripts +
         ", hasJava=" + hasJava +
-        ", branch=" + branch +
         ", stars=" + stars +
         '}';
     }
